4d27e7b13fbaf52baf6bd7ff8d46108d973612f3
Levi <139384471+imursugarmommy@users.noreply.github.com>
2024-03-08 13:09:07 +0100

    Full working game now
    1. random word and choose word working
    2. game over (no lifes) working + stats
    3. game won (word guessed) working
    
    still a couple bugs but working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Game from "./Game.tsx";
-import ChooseWord from "./ChooseWord.tsx";
+import ChooseWord from "./ChooseWord.jsx";
+import GameOver from "./GameOver.tsx";
+import Win from "./Win.tsx";
 
 const App = () => {
+  const [gameStart, setGameStart] = useState(false);
+  const [gameEnd, setGameEnd] = useState(false);
+  const [win, setWin] = useState(false);
+
   const [customWord, setCustomWord] = useState("");
   const [correctLetters, setCorrectLetters] = useState([]);
   const [clickedLetters, setClickedLetters] = useState([]);
   const [letterStyles, setLetterStyles] = useState({});
+  const [lifes, setLifes] = useState(5);
+  const [timer, setTimer] = useState({
+    seconds: 0,
+    minutes: 0,
+  });
+  const [
+    sortedAndFilteredCustomWordLetters,
+    setSortedAndFilteredCustomWordLetters,
+  ] = useState([]);
+
+  useEffect(() => {
+    setSortedAndFilteredCustomWordLetters(
+      customWordInCharacters
+        .slice()
+        .sort()
+        .filter((value, index, self) => self.indexOf(value) === index)
+    );
+  }, [gameStart]);
+
+  useEffect(() => {
+    if (timer.seconds === 60) {
+      setTimer((prevTimer) => ({
+        ...prevTimer,
+        seconds: 0,
+        minutes: prevTimer.minutes + 1,
+      }));
+    }
+  }, [timer.seconds]);
+
+  useEffect(() => {
+    if (!gameStart) return;
+
+    let timerInterval = setInterval(() => {
+      setTimer((prevTimer) => ({
+        ...prevTimer,
+        seconds: prevTimer.seconds + 1,
+      }));
+    }, 1000);
+
+    // ! funktioniert nicht
+    if (!gameEnd) return;
+    clearInterval(timerInterval);
+  }, [gameEnd, gameStart]);
 
   const customWordInCharacters = customWord.toUpperCase().split("");
 
@@ -66,6 +115,29 @@ const App = () => {
           color: "black",
         },
       }));
+    } else {
+      setLifes((prevLifes) => prevLifes - 1);
+
+      if (lifes <= 0) {
+        setLifes(0);
+        setGameEnd(() => true);
+      }
+    }
+
+    checkWinner();
+  }
+
+  useEffect(() => {
+    checkWinner();
+  }, [clickedLetters]);
+
+  function checkWinner() {
+    if (
+      JSON.stringify(correctLetters.slice().sort()) ===
+        JSON.stringify(sortedAndFilteredCustomWordLetters) &&
+      gameStart
+    ) {
+      setWin(() => true);
     }
   }
 
@@ -74,11 +146,23 @@ const App = () => {
       <ChooseWord
         customWord={customWord}
         setCustomWord={setCustomWord}
+        setGameStart={setGameStart}
       />
       <Game
         customWordItem={customWordItem}
         listItem={listItem}
+        lifes={lifes}
       />
+      {lifes === 0 && (
+        <GameOver
+          clickedLetters={clickedLetters}
+          correctLetters={correctLetters}
+          customWord={customWord}
+          setGameEnd={setGameEnd}
+          timer={timer}
+        />
+      )}
+      {win === true && <Win />}
     </div>
   );
 };
